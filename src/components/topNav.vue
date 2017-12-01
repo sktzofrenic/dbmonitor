46aff8e5b61e225e2840c180b16630ec39f1cfbe
sktzofrenic <dan@danwins.com>
2017-12-01 08:27:03 -0500

    Add top nav menu

---
+++ src/components/topNav.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="ui fixed  menu">
+    <div class="ui container">
+        <a href="#" class="header item">
+            <i class="bar chart icon"></i>
+                Database Monitor
+        </a>
+        <div class="right menu">
+            <div class="ui simple dropdown item">
+                <i class="alarm icon"></i>
+                Alerts <i class="dropdown icon"></i>
+                <div class="menu">
+                    <a class="item" href="#">Alert 1</a>
+                    <a class="item" href="#">Alert 2</a>
+                    <a class="item" href="#">Alert 3</a>
+                    <div class="divider"></div>
+                    <a class="item" href="#">See Older</a>
+                </div>
+            </div>
+            <div class="ui simple dropdown item">
+                <i class="setting icon"></i>
+                Settings <i class="dropdown icon"></i>
+                <div class="menu">
+                  <a class="item" href="#">Reset to Defaults</a>
+                    <div class="divider"></div>
+                    <a class="item" href="#">More Preferences</a>
+                </div>
+            </div>
+            <div class="ui simple item">
+                <i class="help circle icon"></i>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'topNav',
+  data() {
+    return {
+      msg: 'DB Monitor'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
