1d7232abfaf2c668f8fc377657f51b820bd55440
sktzofrenic <dan@danwins.com>
2017-12-05 08:56:44 -0500

    Make code conform to ESlint

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,14 +1,14 @@
-<template lang="html">
-    <div v-if="showModal" class="ui modal active">
-        <i class="close icon"></i>
-        <div class="header">
+<template lang='html'>
+    <div v-if='showModal' class='ui modal active'>
+        <i class='close icon'></i>
+        <div class='header'>
             {{ modalData.title }}
         </div>
-        <div class="content">
-            <canvas id="myChart" width="400" height="200px"></canvas>
+        <div class='content'>
+            <canvas id='myChart' width='400' height='200px'></canvas>
         </div>
-        <div class="actions">
-            <div class="ui black deny button" @click="toggleModal({})">
+        <div class='actions'>
+            <div class='ui black deny button' @click='toggleModal({})'>
                 Close
             </div>
         </div>
@@ -54,7 +54,7 @@ export default {
             }]
         } else if (this.modalData.title === '% CPU used by DB') {
             labels = this.activeDatabase.cpuGraph.map(function (value) {
-                return moment(value.date).format("M/D, h:mmA")
+                return moment(value.date).format('M/D, h:mmA')
             }).reverse()
             data = this.activeDatabase.cpuGraph.map(function (value) {
                 return value.value
@@ -68,7 +68,7 @@ export default {
             }]
         } else if (this.modalData.title === 'Disk I/O in kBs') {
             labels = this.activeDatabase.ioGraph.map(function (value) {
-                return moment(value.date).format("M/D, h:mmA")
+                return moment(value.date).format('M/D, h:mmA')
             }).reverse()
             data = this.activeDatabase.ioGraph.map(function (value) {
                 return value.value
@@ -82,7 +82,7 @@ export default {
             }]
         } else if (this.modalData.title === '% Memory Usage') {
             labels = this.activeDatabase.memoryGraph.map(function (value) {
-                return moment(value.date).format("M/D, h:mmA")
+                return moment(value.date).format('M/D, h:mmA')
             }).reverse()
             data = this.activeDatabase.memoryGraph.map(function (value) {
                 return value.value
@@ -97,17 +97,17 @@ export default {
         } else if (this.modalData.title === 'Wait by Type') {
             var typeSet = new Set()
             var dateSet = new Set()
-            var type = this.activeDatabase.sqlBreakdownGraph.map(function (value) {
+            this.activeDatabase.sqlBreakdownGraph.map(function (value) {
                 typeSet.add(value.type)
             })
-            var dates = this.activeDatabase.sqlBreakdownGraph.map(function (value) {
-                dateSet.add(moment(value.date).format("M/D, h:mmA"))
+            this.activeDatabase.sqlBreakdownGraph.map(function (value) {
+                dateSet.add(moment(value.date).format('M/D, h:mmA'))
             })
             var uniqueTypes = Array.from(typeSet)
             labels = Array.from(dateSet).reverse()
 
             var colorObj = {}
-            var colors = uniqueTypes.map(function (color) {
+            uniqueTypes.map(function (color) {
                 colorObj[color] = `${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1}`
             })
 
@@ -116,7 +116,7 @@ export default {
                     label: value,
                     data: labels.map(function (original, index) {
                         var match = vm.activeDatabase.sqlBreakdownGraph.filter(function (old) {
-                            return moment(old.date).format("M/D, h:mmA") === original && old.type === value
+                            return moment(old.date).format('M/D, h:mmA') === original && old.type === value
                         })[0]
                         if (match) {
                             return match.wait
@@ -130,7 +130,6 @@ export default {
                 }
             })
         }
-        var vm = this
         var ctx = document.getElementById('myChart')
         if (ctx) {
             var myChart = new Chart(ctx, {
@@ -161,7 +160,7 @@ export default {
 }
 </script>
 
-<style lang="css" scoped>
+<style lang='css' scoped>
 .modal {
     margin-top: -300px;
 }
